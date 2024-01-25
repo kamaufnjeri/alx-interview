5810d92b67dde4f694af33e4a0f6abc4b31d9aa1
kamaufnjeri <kamaufnjeri2019@gmail.com>
2024-01-26 02:49:21 +0300

    log parsing status code in ascending order and executable

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,35 +1,49 @@
 #!/usr/bin/python3
-"""This script reads lines from stdin in this format
-<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
-and after every 10 lines or keyboard interruption
-it prints File size: <total size>
-<status code>: <number> for every status code"""
+"""HTTP Log Parser
+
+This script reads lines from stdin in the specified format and computes metrics.
+
+Format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
+
+After every 10 lines or keyboard interruption, it prints:
+- Total File Size: <total size>
+- Status Code Counts: <status code>: <number> for every status code
+"""
 
 from sys import stdin
 
-
 try:
-    my_dict = {}
-    total_size = 0
-    for i, line in enumerate(stdin, start=1):
-        parts = line.split(" ")
+    status_counts = {}
+    accumulated_size = 0
+
+    for line_number, log_line in enumerate(stdin, start=1):
+        log_parts = log_line.split(" ")
+
         try:
-            total_size += int(parts[-1])
-            status = int(parts[-2])
-            if status not in my_dict:
-                my_dict[status] = 1
+            file_size = int(log_parts[-1])
+            status_code = int(log_parts[-2])
+
+            accumulated_size += file_size
+
+            if status_code not in status_counts:
+                status_counts[status_code] = 1
             else:
-                my_dict[status] += 1
+                status_counts[status_code] += 1
+
         except (ValueError, IndexError):
             continue
-        my_dict = dict(sorted(my_dict.items()))
-        if i % 10 == 0:
-            print("File size: {}".format(total_size))
-            for key, val in my_dict.items():
-                print("{}: {}".format(key, val))
+
+        status_counts = dict(sorted(status_counts.items()))
+
+        if line_number % 10 == 0:
+            print("Total File Size: {}".format(accumulated_size))
+            for code, count in status_counts.items():
+                print("{}: {}".format(code, count))
+
 except KeyboardInterrupt:
     pass
+
 finally:
-    print("File size: {}".format(total_size))
-    for key, val in my_dict.items():
-        print("{}: {}".format(key, val))
+    print("Total File Size: {}".format(accumulated_size))
+    for code, count in status_counts.items():
+        print("{}: {}".format(code, count))
