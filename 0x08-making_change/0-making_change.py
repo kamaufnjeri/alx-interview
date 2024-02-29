7b97bbeb76f98c29a56c136666c12e4a422c913e
kamaufnjeri <kamaufnjeri2019@gmail.com>
2024-02-29 21:59:16 +0300

    change from coins

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,34 +1,25 @@
 #!/usr/bin/python3
-"""
-Defines a function makeChange to solve the coin change problem.
+"""Change making module.
 """
 
 
 def makeChange(coins, total):
-    """
-    Returns the fewest number of coins needed to meet the given amount total.
-
-    Args:
-        coins (list): List of coin denominations.
-        total (int): Target amount to make up using coins.
-
-    Returns:
-        int: Fewest number of coins needed to meet total.
-             Returns -1 if total cannot be met.
+    """Determines the fewest number of coins needed to meet a given
+    amount total when given a pile of coins of different values.
     """
     if total <= 0:
         return 0
-
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
-    for coin in coins:
-        for amount in range(coin, total + 1):
-            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
-
-    return dp[total] if dp[total] != float('inf') else -1
-
-
-if __name__ == "__main__":
-    print(makeChange([1, 2, 25], 37))  # Output should be 7
-    print(makeChange([1256, 54, 48, 16, 102], 1453))  # Output should be -1
+    rem = total
+    coins_count = 0
+    coin_idx = 0
+    sorted_coins = sorted(coins, reverse=True)
+    n = len(coins)
+    while rem > 0:
+        if coin_idx >= n:
+            return -1
+        if rem - sorted_coins[coin_idx] >= 0:
+            rem -= sorted_coins[coin_idx]
+            coins_count += 1
+        else:
+            coin_idx += 1
+    return coins_count
